50bc578e17493b307355b791645ad9e61729a193
unknown <icaro_300@hotmail.com>
2022-05-09 08:44:54 -0300

    second c

--- ano.js
+++ ano.js
@@ -2,7 +2,8 @@ function calculateAge(nascimento,anoatual){
     let ano = anoatual - nascimento
     let dia = ano * 365 
     let semana = ano * 48 
-    console.log("idade:",ano, "dias:",dia,"semanas:",semana)
+    let mes = ano * 12
+    console.log("idade:",ano,"mese:"mes, "dias:",dia,"semanas:",semana)
     
     
     
